b186bdb38cb47c498bcd080623c9f6afd0bd8e9d
@apicac70 <apicac70@gmail.com>
2024-08-14 09:59:58 +0700

    add:swagger prop in register

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
   const config = new DocumentBuilder()
     .setTitle('App-API')
     .setDescription('Billionairesalesfunnel API description')
     .setVersion('1.0')
     .addTag('App-API')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
--- src/user/dto/register-dto.ts
+++ src/user/dto/register-dto.ts
@@ -1,16 +1,21 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'wBkQJ@example.com' })
   readonly email: string;
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly password: string;
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly name: string;
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly tel: string;
 }
