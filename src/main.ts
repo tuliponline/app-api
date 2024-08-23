5de3c8dafd1217793886b48f7cb6433606259058
@apicac70 <apicac70@gmail.com>
2024-08-23 16:50:58 +0700

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   const options: SwaggerDocumentOptions = {
